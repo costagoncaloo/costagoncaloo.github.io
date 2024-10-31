0af0ac3c96322faa13369a2b6b7994d892ce62ad
Gonçalo Costa <114982994+goncalo-costa-a22107998@users.noreply.github.com>
2024-10-31 11:55:43 +0000

    ultimas alterações

--- lab7/main.js
+++ lab7/main.js
@@ -1,29 +1,28 @@
 // 2
 function changeColor(color) { 
-    document.getElementById('colorBox').style.backgroundColor = color; } 
-
+    document.getElementById('colorBox').style.backgroundColor = color; 
+} 
 
 // 3
 function showText() {
     let inputText = document.getElementById('inputText').value; 
     document.getElementById('displayText').innerText = inputText; 
 
-    
     let colors = ['red', 'blue', 'green', 'yellow'];
     let randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.getElementById('inputText').style.backgroundColor = randomColor;
 }
 
-
 // 4
 function submitColor() { 
     let color = document.getElementById('colorInput').value; 
-    document.getElementById('colorBox').style.backgroundColor = color; } 
-
+    document.getElementById('colorBox').style.backgroundColor = color; 
+} 
 
 // 5
+let countValue = 0; // Mova a variável para fora da função
+
 function count() { 
-    let countValue = 0;
     countValue++; 
     document.getElementById('countDisplay').innerText = countValue; 
 }
