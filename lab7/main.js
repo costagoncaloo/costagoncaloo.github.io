0420d82e4b8899454b8f678a0dd6027ee96b3d39
Gonçalo Costa <114982994+goncalo-costa-a22107998@users.noreply.github.com>
2024-10-31 11:06:40 +0000

    implementação das funções

--- lab7/main.js
+++ lab7/main.js
@@ -1,3 +1,21 @@
-function changeColor(color)  {
-    document.getElementById('colorBox').style.backgroundColor = color;
-}
+// Função para mudar de cor
+function changeColor(color) { 
+    document.getElementById('colorBox').style.backgroundColor = color; } 
+
+
+// 
+function showText() { 
+    let inputText = document.getElementById('inputText').value; 
+    document.getElementById('displayText').innerText = inputText; } 
+
+
+// Function to submit color from input 
+function submitColor() { 
+    let color = document.getElementById('colorInput').value; 
+    document.getElementById('colorBox').style.backgroundColor = color; } 
+
+
+// Função para contagem 
+function count() { 4
+    countValue++; 
+    document.getElementById('countDisplay').innerText = countValue; }
